3472f0c87bc8d305a0af244c5be21e54412b3c49
Squirrel963 <111523440+Squirrel963@users.noreply.github.com>
2026-08-18 14:06:50 +0800

    Add initial implementation of register class and functions

---
+++ ExampleP/main.py
@@ -0,0 +1,23 @@
+#  ____   ____      _     ____      _____                                 _
+# |  _ \ / ___|    / \   / ___|    | ____|__  __  __ _  _ __ ___   _ __  | |  ___
+# | |_) |\___ \   / _ \  \___ \    |  _|  \ \/ / / _` || '_ ` _ \ | '_ \ | | / _ \
+# |  __/  ___) | / ___ \  ___) |   | |___  >  < | (_| || | | | | || |_) || ||  __/
+# |_|    |____/ /_/   \_\|____/    |_____|/_/\_\ \__,_||_| |_| |_|| .__/ |_| \___|
+#                                                                 |_|
+
+class register:
+    version = 1.0
+    commands = {
+        "test":"example.test",
+        "hi":"example.hi",
+        "head":"example.headget"
+    }
+
+def test(api):
+    return str(api)
+
+def hi(api):
+    return {'code':'200','data':'Hi','msg':"Everything is (200)ok"}
+    
+def head(api):
+    return api['client']['request']['head'].get('User-Agent')
